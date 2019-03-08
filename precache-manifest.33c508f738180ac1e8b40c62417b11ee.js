8d506dec06ddf115b119b76a8094f966afbfc34d
dmeneses2013 <dmeneses2013@gmail.com>
2019-03-08 11:12:49 +1100

    Updates

---
+++ precache-manifest.33c508f738180ac1e8b40c62417b11ee.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "97ba41be40460fa8a9c2",
+    "url": "/static/css/main.4cc5869d.chunk.css"
+  },
+  {
+    "revision": "97ba41be40460fa8a9c2",
+    "url": "/static/js/main.97ba41be.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "2e4808c850312d058dc7",
+    "url": "/static/js/2.2e4808c8.chunk.js"
+  },
+  {
+    "revision": "c42c34a71411fbbdbdfa97d808bb3bc5",
+    "url": "/static/media/star.c42c34a7.png"
+  },
+  {
+    "revision": "cb272010ab5088327b50aed68d6a2ca4",
+    "url": "/static/media/iconSidebar.cb272010.png"
+  },
+  {
+    "revision": "0b3dd5f2c93668a33804c79ab9af9591",
+    "url": "/index.html"
+  }
+];
